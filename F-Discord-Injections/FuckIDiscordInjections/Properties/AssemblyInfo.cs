fe38db1fc90292bee2193bddd5d0d00c0eb74d45
InnoPaws <1nv13@proton.me>
2024-06-06 18:42:55 -0400

    Add files via upload

--- F-Discord-Injections/FuckIDiscordInjections/Properties/AssemblyInfo.cs
+++ F-Discord-Injections/FuckIDiscordInjections/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("FuckIDiscordInjections")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A Simple tool to remove discord injections and locked program that malware locked")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("InnoPaws")]
 [assembly: AssemblyProduct("FuckIDiscordInjections")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
